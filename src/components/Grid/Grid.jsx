71ac4c404835e4e3826bb570e0c175e58d9b4aff
Zach Hardesty <zachhardesty@ibm.com>
2019-07-08 13:45:36 -0500

    add aspect ratio to grid

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -5,13 +5,21 @@ import PropTypes from 'prop-types'
 import { GridCol } from './GridCol'
 import { GridRow } from './GridRow'
 
+// @TODO: test me so much
 export const Grid = ({
+	aspectRatio = undefined,
 	fullWidth = false,
 	condensed = false,
 	className = '',
 	children = null,
 }) => (
-	<div className={cx('bx--grid', fullWidth && 'bx--grid--full-width', condensed && 'bx--grid--condensed', className)}>
+	<div className={cx(
+		'bx--grid', fullWidth && 'bx--grid--full-width', condensed && 'bx--grid--condensed',
+		aspectRatio && 'bx--aspect-ratio',
+		aspectRatio && `bx--aspect-ratio--${aspectRatio[0]}x${aspectRatio[1]}`,
+		className
+	)}
+	>
 		{children}
 	</div>
 )
@@ -20,6 +28,8 @@ Grid.Col = GridCol
 Grid.Row = GridRow
 
 Grid.propTypes = {
+	/** input: [width, height] */
+	aspectRatio: PropTypes.oneOf([16, 9], [2, 1], [4, 3], [1, 1]),
 	fullWidth: PropTypes.bool,
 	condensed: PropTypes.bool,
 	className: PropTypes.string,
